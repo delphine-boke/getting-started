03fd66f18458a8317d856a17df36217525590ed0
delphine nyaboke <delphine@sendyit.com>
2022-01-21 15:52:40 +0300

    Add greeting service

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,12 +1,25 @@
 package org.acme.getting.started;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+
+@Path("/hello") //this returns hello on hello endpoint
 public class GreetingResource {
+
+    @Inject  //call it here to be used
+    GreetingService service;
+
+    @GET //specify type of request
+    @Produces(MediaType.TEXT_PLAIN) //specify media type used
+    @Path("/greeting/{name}") //select the path of the greeting service
+    public String greeting(@PathParam String name) {
+        return service.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
